6cbc60cfa4387374b09f616fc211610f5ae0c674
rogozins <rogozins@nokia.com>
2018-11-04 16:32:01 +0100

    First working FindGUnit

---
+++ FindGUnit.cmake
@@ -0,0 +1,86 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+#.rst:
+# FindGTest
+# ---------
+#
+# Locate GUnit C++ Testing Framework
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines the following :prop_tgt:`IMPORTED` targets:
+#
+# ``GUnit::GUnit``
+# The GUnit if found; adds GTest::GTest automatically
+#
+# ``GUnit::Main``
+# Wraps GTest::Main
+#
+#
+# Result variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project:
+#
+# ``GUNIT_FOUND``
+#   Found the GUnit framework
+# ``GUNIT_INCLUDE_DIRS``
+#   the directory containing the GUnit headers
+#
+#
+# Cache variables
+# ^^^^^^^^^^^^^^^
+#
+# The following cache variables may also be set:
+#
+# ``GUNIT_ROOT``
+#   The root directory of the GUnit installation (may also be
+#   set as an environment variable)
+#
+#
+# Example usage
+# ^^^^^^^^^^^^^
+#
+# ::
+#
+#     enable_testing()
+#     find_package(GUnit REQUIRED)
+#
+#     add_executable(foo foo.cc)
+#     target_link_libraries(foo GUnit::GUnit)
+#
+#     add_test(AllTestsInFoo foo)
+
+find_path(GUNIT_INCLUDE_DIR GUnit.h
+    HINTS
+        $ENV{GUNIT_ROOT}/include
+        ${GUNIT_ROOT}/include
+)
+
+mark_as_advanced(GUNIT_INCLUDE_DIR)
+
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GUnit REQUIRED_VARS GUNIT_INCLUDE_DIR)
+
+if(NOT GUNIT_FOUND)
+    return()
+endif()
+
+message("GUnit found: " ${GUNIT_INCLUDE_DIR})
+find_package(GTest REQUIRED)
+
+if (NOT TARGET GUnit::GUnit)
+    add_library(GUnit::GUnit INTERFACE IMPORTED)
+    set_target_properties(GUnit::GUnit PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${GUNIT_INCLUDE_DIR})
+    set_target_properties(GUnit::GUnit PROPERTIES
+        INTERFACE_LINK_LIBRARIES GTest::GTest)
+endif()
+
+if (NOT TARGET GUnit::Main)
+    add_library(GUnit::Main INTERFACE IMPORTED)
+    set_target_properties(GUnit::Main PROPERTIES
+        INTERFACE_LINK_LIBRARIES GTest::Main)
+endif()
